5b88ff54bdb45e4517cf40dfee1cef71e10cad1e
jason_w <brackman@gmail.com>
2017-04-01 12:59:32 -0700

    adding a new image and some work to break down the bytes of the bytes. Looking for clues.

--- challenge_01.py
+++ challenge_01.py
@@ -13,6 +13,7 @@ import os
 from PIL import Image
 import challenge_00
 
+
 def decrypt_message():
     passkey = '{}'.format('DidTheMoonLandingReallyHappen?!?'.lower())
     passkey = '{}'.format('DidTheMoonLandingReallyHappen'.lower())
@@ -22,24 +23,22 @@ def decrypt_message():
     challenge_00.decrypt_openssl(infile=infile_, outfile=outfile_, passphrase=passkey)
 
 
-def get_new_data(im, value):
-    width, height = im.size
-
-    new_data = []
-    for w in range(width):
-        for h in range(height):
-
-            pixel = im.getpixel((h, w))
-            if value == pixel:
-                pixel = 255
-            else:
-                pixel = 0
-            new_data.append(pixel)
-
-    return new_data
+def display_hex_values(path):
+    with open(path, 'rb') as handle:
+        while True:
+            chunk = handle.read(100)
+            if not chunk:
+                break
+            stuff = ''.join(chr(item) for item in chunk)
+            print(stuff)
+            if 'hack' in stuff:
+                print(stuff)
 
 
-def explore_pixel_values(path_01, path_02):
+
+
+
+def generate_differences_in_white_pixel_values(path_01, path_02):
 
     im_01 = Image.open(path_01)
     im_02 = Image.open(path_02)
@@ -53,22 +52,42 @@ def explore_pixel_values(path_01, path_02):
             pixel_02 = im_02.getpixel((h, w))
 
             if pixel_01 == pixel_02:
-                pixels.append((0, 0, 0))
+                pixels.append(pixel_01)
             else:
                 pixels.append((250, 250, 250))
 
     temp_im = Image.new(im_01.mode, im_01.size)
     temp_im.putdata(pixels)
-    temp_im.save('./Challenges/Challenge 1/generated_content/output.jpg')
+    temp_im.save('./Challenges/Challenge 1/generated_content/output_01.jpg')
 
-def convert_to_grayscale(path):
-    im = Image.open(path)
-    im = im.convert('1')
-    im.save('grayscale_test.bmp')
+
+# def convert_to_grayscale(path):
+#     im = Image.open(path)
+#     im = im.convert('1')
+#     im.save('grayscale_test.bmp')
+
+
+# def get_new_data(im, value):
+#     width, height = im.size
+#
+#     new_data = []
+#     for w in range(width):
+#         for h in range(height):
+#
+#             pixel = im.getpixel((h, w))
+#             if value == pixel:
+#                 pixel = 255
+#             else:
+#                 pixel = 0
+#             new_data.append(pixel)
+#
+#     return new_data
+
 
 if __name__ == "__main__":
     path = './Challenges/Challenge 1/AldrinApollo11'
     path02 = './Challenges/Challenge 1/generated_content/downloaded_from_internet.jpg'
-    # explore_pixel_values(path, path02)
 
-    decrypt_message()
+    display_hex_values(path)
+    # generate_differences_in_white_pixel_values(path, path02)
+    # decrypt_message()
