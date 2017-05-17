04e3282a1c0f09cd1255fa0cb3e2e5feec52e107
jasonbrackman <brackman@gmail.com>
2017-05-17 09:01:19 -0700

    random attempts to discover anything new

--- Challenges/Challenge 8/generated_content/get_low_bits.py
+++ Challenges/Challenge 8/generated_content/get_low_bits.py
@@ -30,6 +30,12 @@ def pprint_16bit_lsb(raw_data):
     _group_and_print(collection, bits=16)
 
 
+def pprint_valid_ascii_characters(raw_data):
+    for item in raw_data:
+        if 32 <= item <= 122:
+            print(chr(item), end='')
+
+
 def _group_and_print(collection, bits):
     for index in range(0, len(collection), bits):
         current = collection[index: index + bits]
@@ -37,46 +43,39 @@ def _group_and_print(collection, bits):
         current = int(current, 2)
         print(chr(current), end='')
 
+if __name__ == "__main__":
+    with wave.open('../047b704a141707ec15a8171ab1d37dbd.wav', 'rb') as sound:
+        num_channels = sound.getnchannels()
+        sample_width = sound.getsampwidth()
+        num_frames = sound.getnframes()
+        comp_type = sound.getcomptype()
 
-with wave.open('../047b704a141707ec15a8171ab1d37dbd.wav', 'rb') as sound:
-    num_channels = sound.getnchannels()
-    sample_width = sound.getsampwidth()
-    num_frames = sound.getnframes()
-    comp_type = sound.getcomptype()
-    num_samples = num_frames * num_channels
-    fmt = "{}h".format(num_samples)
-    # The least possible value for a sample in the sound file
-    min_sample = -(1 << 15)
+        num_samples = num_frames * num_channels
+        fmt = "{}h".format(num_samples)
+        # The least possible value for a sample in the sound file
+        min_sample = -(1 << 15)
 
-    print('MinSample: ', min_sample)
-    print('fmt: ', fmt)
-    print('channels: ', num_channels)
-    print('sample_width: ', sample_width)
-    print('num_frames: ', num_frames)
-    print('num_samples: ', num_samples)
-    print('comp_type: ', comp_type)
+        print('MinSample: ', min_sample)
+        print('fmt: ', fmt)
+        print('channels: ', num_channels)
+        print('sample_width: ', sample_width)
+        print('num_frames: ', num_frames)
+        print('num_samples: ', num_samples)
+        print('comp_type: ', comp_type)
 
-    # Put all the samples from the sound file into a list
-    raw_data = list(struct.unpack(fmt, sound.readframes(num_frames)))
+        # Put all the samples from the sound file into a list
+        raw_data = list(struct.unpack(fmt, sound.readframes(num_frames)))
 
-    # split the stereo into left and right
-    l = [r for index, r in enumerate(raw_data) if index % 2 == 0]
-    r = [r for index, r in enumerate(raw_data) if index % 2 == 1]
+        # split the stereo into left and right
+        l = [r for index, r in enumerate(raw_data) if index % 2 == 0]
+        r = [r for index, r in enumerate(raw_data) if index % 2 == 1]
 
-    # Left & Right channel info should be equal for this challenge
-    assert l == r
-    # print(collections.Counter(r).most_common(5))
+        # Left & Right channel info should be equal for this challenge
+        assert l == r
+        # print(collections.Counter(r).most_common(5))
 
-    pprint_7bit_lsb(r)
-    # pprint_16bit_lsb(r)
-    # pprint_negative_positive_as_bits(r)
-
-
-    # for item in raw_data:
-    #     if item > 0:
-    #         print(hex(item), end='')
-    # for item in raw_data:
-    #     if item > 0:
-    #         print(chr(item), end='')
-        # print(chr(item), end='')
-
+        # Attempts at revealing something ---
+        # pprint_7bit_lsb(r)
+        # pprint_16bit_lsb(r)
+        # pprint_negative_positive_as_bits(r)
+        # pprint_valid_ascii_characters(r)
--- Challenges/Challenge 8/generated_content/musings.py
+++ Challenges/Challenge 8/generated_content/musings.py
@@ -20,6 +20,7 @@ import os
 #             print('{} Results:'.format(module))
 #             for result in module.results:
 #                 print(result.file.name, result.offset, result.description)
+import string
 
 def carve_binary():
     stuff = []
@@ -62,23 +63,38 @@ def carve_binary2():
     # # all = ''.join(stuff)
 
 
-def carve_binary3(start_byte=44, bits=7, mask=0b1111111):
+def carve_binary3(stuff, start_byte=0, bits=7, mask=0b1111111):
+    # clipped = [item[-1] for item in stuff[39004:]]
+    end_byte = len(stuff)
+
+    end_byte = start_byte + 500
+    collection = []
+    for index in range(start_byte, end_byte, bits):
+        new_word = [bin(mask & int(i, 2))[-1] for i in stuff[index:index+bits]]
+        new_letter = int(''.join(new_word), 2)
+        test_character = chr(new_letter)
+        if test_character in string.printable:
+            collection.append(test_character)
+
+    if len(collection) >= 30:
+        # print("Could Be The One at start_byte: {} bit:{}".format(start_byte, bits))
+        print(start_byte, len(collection), ''.join(collection[0:80]))
+
+
+    # print(len(collection), start_byte,  ''.join(collection[0:80]))
+
+
+def get_binary_representation():
     stuff = []
     with open('../047b704a141707ec15a8171ab1d37dbd.wav', 'rb') as handle:
         for line in handle:
             for letter in line:
-                stuff.append(letter)
+                stuff.append(bin(letter))
 
-    for index in range(start_byte, len(stuff), bits):
-        # print(chr(stuff[index]), end='')
-
-        goods = [i for i in stuff[index:index+bits]]
-        new_word = [bin(mask & i)[-1] for i in goods]
-        new_letter = int(''.join(new_word), 2)
-        print(chr(new_letter), end='')
+    return stuff
 
 
-def domo_test():
+def domo_t():
     x = "domoarigatomrroboto"
     y = {'d': 4,
          'o': 15,
@@ -94,7 +110,10 @@ def domo_test():
 
 if __name__ == "__main__":
     # binwalk_test()
-    carve_binary3()
+    stuff = get_binary_representation()
+    for index in range(32000, 42320):
+        #print("STARTING NEW ROUND: {}".format(index))
+        carve_binary3(stuff, start_byte=index)
 
 
 
